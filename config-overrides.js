b2c8731eb2f43fc07cd49323adc53809acacdc8c
muhrusdi <muh.rusdi@docotel.com>
2019-10-09 19:17:38 +0800

    first-commit

---
+++ config-overrides.js
@@ -0,0 +1,46 @@
+const {
+  override,
+  disableEsLint,
+  addBabelPresets,
+  addWebpackAlias,
+  addBabelPlugins,
+  addWebpackResolve,
+} = require("customize-cra");
+const path = require("path");
+
+const aliases = {
+  "@components": path.resolve(__dirname, "src/components"),
+  "@containers": path.resolve(__dirname, "src/containers"),
+  "@hooks": path.resolve(__dirname, "src/hooks"),
+  "@hocs": path.resolve(__dirname, "src/hocs"),
+  "@images": path.resolve(__dirname, "src/images"),
+  "@utils": path.resolve(__dirname, "src/utils"),
+  "@layout": path.resolve(__dirname, "src/containers/layout"),
+};
+
+module.exports = override(
+  // disable eslint in webpack
+  disableEsLint(),
+  ...addBabelPresets(
+    "@babel/preset-env",
+    "@babel/preset-react",
+  ),
+  ...addBabelPlugins(
+    "react-hot-loader/babel",
+    "babel-plugin-styled-components"
+  ),
+
+  /**
+   * add path alias
+   */
+  addWebpackAlias(aliases),
+
+  /**
+   * resolve extention
+   */
+  addWebpackResolve(
+    {
+      extensions: [".js", ".jsx", ".css"]
+    }
+  ),
+);
---
+++ src/containers/app/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader/root"
+
+const App = () => {
+  return (
+    <div>
+      app
+    </div>
+  )
+}
+
+export default hot(App);
