e6845116ecaf0576f20baf204deb83909a69ce04
muhrusdi <muh.rusdi@docotel.com>
2019-11-07 11:23:58 +0800

    fix path client side

--- hyperion/index.js
+++ hyperion/index.js
@@ -12,7 +12,7 @@ const PORT = IS_PROD ? process.env.PORT_PROD : process.env.PORT_DEV || 3000;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
